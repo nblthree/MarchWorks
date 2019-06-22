b6b43212806e32a78302af2f7e012e69ddfa1154
EdelWolle <i-bn@live.fr>
2019-06-22 11:51:27 +0100

    switch to yarn

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
-module.exports = {
+const nextMDX = require('@next/mdx');
+const bundleAnalyzer = require('@next/bundle-analyzer');
+
+
+const withMDX = nextMDX({
+  extension: /[/\\](pages|blog)[/\\](.+)\.mdx?$/,
+});
+
+const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
+
+const nextConfig = {
   target: 'serverless',
+  pageExtensions: ['jsx', 'js', 'mdx'],
+  experimental: {
+    flyingShuttle: true,
+  },
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL || 'https://march-works.marchworks.now.sh',
+    SITE_NAME: 'MarchWorks',
+  },
 };
+
+module.exports = withMDX(withBundleAnalyzer(nextConfig));
