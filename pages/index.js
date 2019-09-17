90ee790cdfad55bccc8a277f72e13267842970c7
MarchWorks <i-bn@live.fr>
2019-09-17 18:25:36 +0100

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ class Index extends React.Component {
                   notified whenever a new episode is available
                 </p>
                 <div className='download'>
-                  <a href='https://github.com/MarchWorks/AniTV/releases/latest/download/window.x64.zip'>
+                  <a href='https://github.com/MarchWorks/AniTV/releases/latest/download/windows.x64.zip'>
                     <button>Download For Windows x64</button>
                   </a>
                 </div>
